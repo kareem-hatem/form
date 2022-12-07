ce8d6a18e6337b9900a58ba97d6438ede419dd82
Karim Hatem <119761193+kareem-hatem@users.noreply.github.com>
2022-12-07 17:00:36 +0200

    added more share options and some other little things

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function myFunction1() {
   alert("Your answers will be sent to the creator");
 }
 
@@ -6,13 +6,13 @@ const modal = document.querySelector(".modal");
 
 function openModal() {
   modal.style.display = "flex";
-  modal.classList.add('fade-in');
-  modal.classList.remove('fade-out');
+  modal.classList.add("fade-in");
+  modal.classList.remove("fade-out");
 }
 
 function closeModal() {
-  modal.classList.add('fade-out');
-  modal.classList.remove('fade-in');
+  modal.classList.add("fade-out");
+  modal.classList.remove("fade-in");
   setTimeout(() => {
     modal.style.display = "none";
   }, 500);
@@ -22,26 +22,52 @@ const more = document.querySelector(".more");
 
 function openModalForMore() {
   more.style.display = "flex";
-  more.classList.add('fade-in');
-  more.classList.remove('fade-out');
+  more.classList.add("fade-in");
+  more.classList.remove("fade-out");
 }
 
 function closeModalForMore() {
-  more.classList.add('fade-out');
-  more.classList.remove('fade-in');
+  more.classList.add("fade-out");
+  more.classList.remove("fade-in");
   setTimeout(() => {
     more.style.display = "none";
   }, 500);
 }
 
-const loader = document.querySelector(".loader")
+const loader = document.querySelector(".loader");
 
 const topContainer = document.querySelector(".top-container");
 const loaderContainer = document.querySelector(".loader-container");
+const loaderText = document.querySelector(".loader-text");
 
 function showPage() {
-  topContainer.style.display = 'unset';
-  loaderContainer.style.display = 'none';
+  topContainer.style.display = "unset";
+  loaderContainer.style.display = "none";
+}
+function displayText() {
+  loaderText.innerHTML = "Finished Loading";
+}
+function displayText2() {
+  loaderText.innerHTML = "Opening website...";
 }
 
-setTimeout(showPage, 2000);
+setTimeout(showPage, 5300);
+
+setTimeout(displayText, 4600);
+
+setTimeout(displayText2, 5000);
+
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
