b15e60d56f55254d661bb148cfab40e4c7a63ddb
Karim Hatem <119761193+kareem-hatem@users.noreply.github.com>
2022-12-08 18:54:20 +0200

    deploy

--- src/script.js
+++ src/script.js
@@ -20,29 +20,17 @@ function closeModal() {
 
 const more = document.querySelector(".more");
 
-function openModalForMore() {
-  more.style.display = "flex";
-  more.classList.add("fade-in");
-  more.classList.remove("fade-out");
-}
-
-function closeModalForMore() {
-  more.classList.add("fade-out");
-  more.classList.remove("fade-in");
-  setTimeout(() => {
-    more.style.display = "none";
-  }, 500);
-}
-
 const loader = document.querySelector(".loader");
 
 const topContainer = document.querySelector(".top-container");
 const loaderContainer = document.querySelector(".loader-container");
 const loaderText = document.querySelector(".loader-text");
+const body = document.querySelector("body")
 
 function showPage() {
   topContainer.style.display = "unset";
   loaderContainer.style.display = "none";
+  body.style.backgroundColor = "#05c46b"
 }
 function displayText() {
   loaderText.innerHTML = "Finished Loading";
@@ -51,11 +39,22 @@ function displayText2() {
   loaderText.innerHTML = "Opening website...";
 }
 
-setTimeout(showPage, 5300);
 
-setTimeout(displayText, 4600);
+function test(){
+  if (confirm ('You seem to like this website, would you like to contact the creator feedback?')) 
+  if(confirm(window.open('https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcSHwQlmCSsdTSkpxxtRhkMvhfXZmWxKwZbCRPzfKKlrQLnqxsdBmJZBQTmXxlsfNrSGrxCJG')))
+  alert("");
+ else{
+  alert("Thanks for your feedback!")
+ }
+}
+setTimeout(displayText, 2800);
 
-setTimeout(displayText2, 5000);
+setTimeout(displayText2, 3000);
+
+setTimeout(showPage, 3100);
+
+setTimeout(test, 220000);
 
 function copy() {
   // Get the text field
@@ -69,5 +68,5 @@ function copy() {
   navigator.clipboard.writeText(copyText.value);
 
   // Alert the copied text
-  alert("Copied the URL to clipboard successfully");
+  alert("Copied the URL to clipboard successfully")
 }
